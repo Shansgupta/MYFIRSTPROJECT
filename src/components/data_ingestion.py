37247137b589261d59318025f68298e864021c90
Shansgupta <22ucc094@lnmmit.ac.in>
2025-07-27 14:06:57 +0530

    Restored .gitignore file

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -45,7 +45,7 @@ if __name__ == "__main__":
   obj = DataIngestion()  
   train_data ,test_data = obj.initiate_data_ingestion()
   data_transformation=DataTransformation()
-  train_arr ,test_arr = data_transformation.initiate_data_tranformation(train_data,test_data)
+  train_arr,test_arr=data_transformation.initiate_data_tranformation(train_data,test_data)
   modeltrainer = ModelTrainer()
   print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
            
--- src/utils.py
+++ src/utils.py
@@ -6,8 +6,9 @@ import sys
 import numpy as np
 from src.exception import CustomException
 import pandas as pd
-import dill # type: ignore ## actually help to create the pickle file 
+import dill  ## actually help to create the pickle file 
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path,obj):
     try :
@@ -28,7 +29,14 @@ def evaluate_models(X_train,y_train,X_test,y_test,models,params):
 
       for i in range(len(list(models))):
         model = list(models.values())[i]
-        model.fit(X_train, y_train) # Train model
+
+        ## model.fit(X_train, y_train) # Train model
+        para=params[list(models.keys())[i]]
+        gs = GridSearchCV(model,para,cv=3)
+        gs.fit(X_train,y_train)
+
+        model.set_params(gs.best_params_)  ## applies best parameter to the original model
+        model.fit(X_train,y_train)
 
                   # Make predictions
         y_train_pred = model.predict(X_train)
@@ -46,7 +54,15 @@ def evaluate_models(X_train,y_train,X_test,y_test,models,params):
     except Exception as e :
       raise CustomException(e,sys)
      
-    
+def load_object(file_path):
+   try:
+      with open (file_path,"rb") as file_obj:
+        return dill.load(file_obj)
+       
+   except Exception as e :
+      raise CustomException(e,sys)     
+   
+   
    ## print(list(models.keys())[i])
     ## model_list.append(list(models.keys())[i])    
 
