6f795173f901d36eab3bbfccf09bb6f7d01b2bf7
Shansgupta <22ucc094@lnmmit.ac.in>
2025-07-27 17:17:41 +0530

    For deployment

--- application.py
+++ application.py
@@ -1,11 +1,15 @@
 from flask import Flask ,request,render_template
 import numpy as np 
 import pandas as pd 
-
+import logging
 from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import CustomData,PredictPipeline
+import traceback
+
+logging.basicConfig(level=logging.DEBUG)
 
 application = Flask(__name__)
+application.bebug = True
 app =  application
 
 @app.route('/')
@@ -16,22 +20,28 @@ def predict_datapoint():
     if request.method == 'GET':
         return render_template('home.html')
     else:
-        data = CustomData(
-            gender = request.form.get('gender'),
-            race_ethnicity= request.form.get('race_ethnicity'),
-            parental_level_of_education= request.form.get('parental_level_of_education'),
-            lunch = request.form.get('lunch'),
-            test_preparation_course=request.form.get('test_preparation_course'),
-            reading_score= float(request.form.get('reading_score')),
-            writing_score= float (request.form.get('writing_score'))
-        )
+        try :
+               data = CustomData(
+               gender = request.form.get('gender'),
+              race_ethnicity= request.form.get('race_ethnicity'),
+               parental_level_of_education= request.form.get('parental_level_of_education'),
+               lunch = request.form.get('lunch'),
+               test_preparation_course=request.form.get('test_preparation_course'),
+               reading_score= float(request.form.get('reading_score')),
+                writing_score= float (request.form.get('writing_score'))
+                )
 
-        pred_df = data.get_data_as_data_frame()
-        print(pred_df)
+               pred_df = data.get_data_as_data_frame()
+               print(pred_df)
 
-        predict_pipeline = PredictPipeline()
-        results = predict_pipeline.predict(pred_df)
-        return render_template ('home.html',results=results[0])
-         
+               predict_pipeline = PredictPipeline()
+               results = predict_pipeline.predict(pred_df)
+               return render_template ('home.html',results=results[0])
+            
+        except Exception as e:
+                
+            print("❌ Exception occurred:")
+            traceback.print_exc()
+            return f"An internal error occurred: {str(e)}", 500
 if __name__ == "__main__" :
     app.run(host = "0.0.0.0")        
